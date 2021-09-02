e9752b5613b9aaa8bf7f6f25dafb4d7960b233c0
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-02 22:56:30 +0530

    employeewage1

---
+++ Uc1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS.OopsConcept
+{
+    class Uc1
+    {
+            static void Main(string[] args)
+            {
+                int ISPresent = 1;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == ISPresent)
+                    Console.WriteLine("prsent");
+                else
+                    Console.WriteLine("Abs");
+                Console.Read();
+            }
+        }
+    }
+
+
+
